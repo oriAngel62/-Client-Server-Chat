b0977718aa25ed026db754917af6f0579da36b23
Ori <oriangel62@gmail.com>
2022-04-05 12:36:57 +0300

    organize structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
 import ContactItem from "./contactItem/ContactItem";
+import React, { useState } from "react";
+import { render } from "react-dom";
 
 function App() {
+    const listMessages = [
+        {
+            sender: "a",
+            type: "text",
+            context: "somthing intrseting",
+            time: "10 min ago",
+        },
+        {
+            sender: "a",
+            type: "text",
+            context: "somthing intrseting",
+            time: "10 min ago",
+        },
+    ];
     const contactList = [
-        { name: "Ori1", lastMessage: "aaa", time: "1 min ago" },
-        { name: "David", lastMessage: "aaa", time: "1 min ago" },
-        { name: "David2", lastMessage: "aaa", time: "1 min ago" },
-        { name: "David2", lastMessage: "aaa", time: "1 min ago" },
+        { name: "Ori1", listMessages },
+        { name: "David", listMessages },
     ];
 
     const contactMap = contactList.map((contact, key) => {
@@ -19,11 +33,40 @@ function App() {
             />
         );
     });
+    const [list, setList] = useState(contactList);
+    const [name, setName] = useState("");
+
+    function handleChange(event) {
+        setName(event.target.value);
+    }
+
+    function handleAdd() {
+        const newList = contactList.concat({
+            name: "o",
+            listMessages,
+        });
+
+        setList(newList);
+        setName("");
+
+        console.log(newList);
+        console.log(list);
+    }
+
     return (
         <div className="container -fluid">
             <div className="row">
                 <div className="col-3">
-                    <button onClick={this.addContact}>click</button>
+                    <div>
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={handleChange}
+                        />
+                        <button type="button" onClick={handleAdd}>
+                            Add
+                        </button>
+                    </div>
                     {contactMap}
                     <div className="col-9">
                         <div className="chat">One of three columns12</div>
