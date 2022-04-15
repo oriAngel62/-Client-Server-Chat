758b7ef46e979589138ea39c2cbbdc277e5d8fc6
Ori <oriangel62@gmail.com>
2022-04-15 11:09:02 +0300

    add id and src for db, work on switch chathistory between contacts

--- client/src/App.js
+++ client/src/App.js
@@ -35,13 +35,27 @@ function App() {
         },
     ];
     const contactList = [
-        { name: "tome", listMessages },
-        { name: "David", listMessages },
+        {
+            id: 1,
+            src: "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp",
+            name: "tome",
+            listMessages,
+        },
+        {
+            id: 2,
+            src: "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp",
+            name: "David",
+            listMessages,
+        },
     ];
-
     const [list, setList] = useState(contactList);
-    const handleCallback = (childData) => {
-        console.log(childData.name);
+    const [chatHistory, setChatHistory] = useState("");
+    const callbackContactItem = (childData) => {
+        for (let i = 0; i < list.length; i++) {
+            if (list[i].id == childData.id) setChatHistory(childData);
+        }
+    };
+    const callbackPopUp = (childData) => {
         if (childData.name !== "") {
             handleAdd(childData.name);
         }
@@ -51,12 +65,7 @@ function App() {
         return (
             <ContactItem
                 contactItem={contact}
-                // name={contact.name}
-                // lastMessage={
-                //     contact.listMessages[listMessages.length - 1].context
-                // }
-                // time={contact.listMessages[listMessages.length - 1].time}
-                key={key}
+                sendDataToParent={callbackContactItem}
             />
         );
     });
@@ -67,17 +76,25 @@ function App() {
             listMessages,
         });
         setList(newList);
-        console.log(contactList);
+        // console.log(contactList);
+    }
+
+    function showChatHistory() {
+        return <ChatHistory contact={chatHistory} />;
     }
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-3">
-                    <Popup sendDataToParent={handleCallback} />
+                    <Popup sendDataToParent={callbackPopUp} />
                     <div className="scroll">{contactMap}</div>
                 </div>
                 <div className="col-9 vh-100">
-                    <ChatHistory contact={{ name: "Ori1", listMessages }} />
+                    {chatHistory != "" ? (
+                        <ChatHistory contact={chatHistory} />
+                    ) : (
+                        <p className="mb-1">Choose Contact to talk with</p>
+                    )}
                 </div>
             </div>
         </div>
--- client/src/contactItem/ContactItem.js
+++ client/src/contactItem/ContactItem.js
@@ -2,51 +2,13 @@ import ChatHistory from "../chatHistory/ChatHistory";
 import "./ContactItem.css";
 import React, { useState } from "react";
 
-function ContactItem({ contactItem }) {
-    const [ChatHistory, setChatHistory] = useState(false);
-
-    // const [count, setCount] = useState(0);
-    // useEffect(() => {
-    //     // Update the document title using the browser API
-    //     document.title = `You clicked ${count} times`;
-    // });
-    // console.log(
-    //     contactItem.listMessages[contactItem.listMessages.length - 1].time
-    // );
-    // <ChatHistory contactList={contactItem}
-    // function handleClick(contactItem) {
-    //     return (
-    //         <div className="game">
-    //             <p>dlaskdklasjdk</p>
-    //         </div>
-    //     );
-
-    //     // return <ChatHistory contactList={contactItem} />;
-    // }
-
-    function showText() {
-        console.log(
-            contactItem.listMessages[contactItem.listMessages.length - 1]
-                .type == "text"
-        );
-        if (
-            contactItem.listMessages[contactItem.listMessages.length - 1]
-                .type == "text"
-        )
-            return (
-                <p className="mb-1">
-                    contactItem.listMessages[ contactItem.listMessages.length -
-                    1 ].context
-                </p>
-            );
-    }
-    function handleClick(contactItem) {
-        return <p>dasd</p>;
-    }
-
-    const numImg = Math.floor(Math.random() * (8 - 1 + 1) + 1).toString();
+function ContactItem({ contactItem, sendDataToParent }) {
+    // const numImg = Math.floor(Math.random() * (8 - 1 + 1) + 1).toString();
+    // const srcImg1 =
+    //     "https://www.bootdey.com/img/Content/avatar/avatar" + numImg + ".png";
     const srcImg =
-        "https://www.bootdey.com/img/Content/avatar/avatar" + numImg + ".png";
+        "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava6-bg.webp";
+
     return (
         <div className="contact">
             <div className="list-group">
@@ -61,10 +23,12 @@ function ContactItem({ contactItem }) {
                     // data-target="#learnMore"
                     // onClick={showChatHistory(contactItem)}
 
-                    onClick={() => setChatHistory(true)}
+                    onClick={() => {
+                        sendDataToParent(contactItem);
+                    }}
                 >
                     <div className="d-flex w-100 justify-content-between">
-                        <img src={srcImg} width="80pxd"></img>
+                        <img src={contactItem.src} width="80pxd"></img>
                         <h4 className="mb-1">{contactItem.name}</h4>
                         <br></br>
                         <small>
